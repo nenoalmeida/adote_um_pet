e8e55047df0d88cb9799087c5771b4229cf10a3e
Neno Almeida <nenoxvx@icloud.com>
2016-09-23 01:26:51 +0000

    Adding device route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 rails g controllerRails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   get 'welcome/index'
   root 'welcome#index'
 
 
   resources :pets
 end
+
